3d23e7428388dc032b19f57c060614c75a43f52c
PoddubniySerg <poddubniysergey1981@gmail.com>
2023-03-11 14:35:37 +0300

    Task 2 is completed.

---
+++ src/task2/stats/accumulator/StatsAccumulatorImpl.java
@@ -0,0 +1,41 @@
+package task2.stats.accumulator;
+
+public class StatsAccumulatorImpl implements StatsAccumulator {
+
+    private int count;
+    private int min;
+    private int max;
+    private int sum;
+
+    @Override
+    public void add(int value) {
+        ++count;
+        sum += value;
+        if (value < min) {
+            min = value;
+        }
+        if (value > max) {
+            max = value;
+        }
+    }
+
+    @Override
+    public int getMin() {
+        return min;
+    }
+
+    @Override
+    public int getMax() {
+        return max;
+    }
+
+    @Override
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public Double getAvg() {
+        return (double) sum / count;
+    }
+}
